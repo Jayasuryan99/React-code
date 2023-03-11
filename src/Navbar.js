76f75248ab84dd55da701cb4c174dae4186c1e8d
Jayasuryan <127474567+Jayasuryan99@users.noreply.github.com>
2023-03-11 18:44:34 +0530

    Add files via upload

---
+++ src/Navbar.js
@@ -0,0 +1,84 @@
+
+
+import React from 'react';
+import Group1 from './assests/Group1.webp';
+import Group from './assests/Group.webp';
+import Group2 from './assests/Group2.webp';
+import Group5 from './assests/Group5.webp';
+import Group6 from './assests/Group6.png';
+import Group7 from './assests/Group7.webp';
+import './navbar.css';
+import Charts from './charts';  
+import Dashboard from './dashboard';
+import './Dashboard.css'
+  
+function Navbar() {
+
+
+	return (
+		<>
+		<div className='home-page'>
+			<nav className='navbar'>
+				<img alt='React-images' id='img'
+					src={Group}/>
+
+				<br/>
+				<div className='sub-bar'>
+					<div className='nav-links'>
+						<a>
+							<img src={Group1}
+								alt='React-images'/>
+						</a>
+						<p>Dashboard</p>
+					</div>
+
+					<div className='nav-links'>
+						<a href='#script'><img src={Group2}
+								alt='React-images'/>
+						</a>
+						<p>CIQ&Scripting</p>
+					</div>
+
+					<div className='nav-links'>
+						<a><img src={Group5}
+								alt='React-images'/>
+						</a>
+						<p>
+							UTRAN
+						</p>
+					</div>
+
+					<div className='nav-links'>
+						<a><img src={Group6}
+								alt='React-images'/></a>
+						<p>
+							Integration
+						</p>
+					</div>
+
+					<div className='nav-links'>
+						<a><img src={Group7}
+								alt='React-images'/>
+						</a>
+						<p>
+							KPI Montoring
+						</p>
+					</div>
+					
+					<span className='border'></span>
+			
+				</div>
+                        <div className='dashboard'>
+                           <Charts />
+						  </div>
+						  <div className='secondpage'>
+							<Dashboard />
+							</div>                       													               
+                         					                         													
+			</nav>
+           </div>
+		</>
+	)
+}
+
+export default Navbar
